ed8fc53a3edb0f2372aeaf12fd1a8ec2acb35883
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2022-08-26 17:09:21 -0300

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,23 @@
+def BubbleSort(meuVetor, N):
+
+    #trocou = true
+    swapped = True
+
+    #while true:
+    while swapped == True:
+      #trocou = false
+      swapped = False
+
+      for i in range(0, N-1):
+          if meuVetor[i] > meuVetor[i + 1]:
+              meuVetor[i], meuVetor[i + 1] = meuVetor[i + 1], meuVetor[i]
+              #trocou = false
+              swapped = True
+
+meuVetor = [-82, 23, -31, 44, -38, -94, 32, -71, -93, 38, -9, -95, -60, 17, -1]
+
+N = len(meuVetor)
+
+BubbleSort(meuVetor, N)
+
+print(meuVetor)
