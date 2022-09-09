f05e8577a251b9acc212d928143547ee2b3c77dd
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2022-09-09 18:42:26 -0300

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -15,8 +15,8 @@ def Particiona(vetor, Inicio, Fim):
             #trocar com o maior elemento apontado popor i
             i = i + 1
 
-        #trocando o elemento em i com o elemento em j
-        (vetor[i], vetor[j]) = (vetor[j], vetor[i])
+            #trocando o elemento em i com o elemento em j
+            (vetor[i], vetor[j]) = (vetor[j], vetor[i])
 
     #Trocar o pipvo com o maior elemento especificado por i
     (vetor[i+1], vetor[Fim]) = (vetor[Fim], vetor[i+1])
@@ -27,14 +27,21 @@ def Particiona(vetor, Inicio, Fim):
 #Função Principal
 def QuickSort(vetor, Inicio, Fim):
     if (Inicio < Fim):
-        Pi = Particiona(vetor, Inicio, Fim)
-        QuickSort(vetor, Inicio, Pi-1)
-        QuickSort(vetor, Pi+1, Fim)
+
+        #Achar o pivo, fazendo com que os elementos menores que o pivo fiquem
+        #ao lado esquerdo, e os maiores ao lado direito do pivo
+        pivo = Particiona(vetor, Inicio, Fim)
+
+        #Chamada recursiva para a esquerda do pivo
+        QuickSort(vetor, Inicio, pivo-1)
+
+        #Chamada recursiva para a direita do pivo
+        QuickSort(vetor, pivo+1, Fim)
         
 #MAIN
 vetor = [-82, 23, -31, 44, -38, -94, 32, -71, -93, 38, -9, -95, -60, 17, -1]
 TamVetor = len(vetor)
 
-QuickSort(TamVetor, 0, TamVetor-1)
+QuickSort(vetor, 0, TamVetor-1)
 
 print(vetor)
